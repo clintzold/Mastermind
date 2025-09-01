81706d3621eabb4f1128ed867197debea1594ce4
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-09-01 09:59:21 -0600

    Create Computer object that can play code maker or breaker
    Gave Computer objects an extra instance variable called
    @options. This is four nested arrays of code options that
    are randomly selected by the computer to create its own code.
    
    Added a method that eliminates past guesses from @options as
    the game progresses and keeps exact matches.
    
    As the game progresses, the computer will 'get smarter' as it
    narrows down on the correct code.

--- lib/gameplay_ai.rb
+++ lib/gameplay_ai.rb
@@ -1,18 +1,43 @@
+require 'pry-byebug'
 class Computers
-  include Gameplay
-  attr_accessor :code, :name
+  #include Gameplay
+  attr_accessor :code, :name, :options
 
   def initialize
     @code = []
     @name = ''
+    @options = 
+        [ ['red', 'blue', 'green', 'pink'], ['red', 'blue', 'green', 'pink'], 
+         ['red', 'blue', 'green', 'pink'], ['red', 'blue', 'green', 'pink'] ]
   end
 
+  #Adjusts random selection to match changing option sizes as Computer calculates guesses
   def enter_code
+
     code = []
-      4.times do |i|
-      options = ['red', 'blue', 'green', 'pink']
-      choice = rand(4)
-      code << options[choice]
+    options = self.options
+    options.each do |item|
+      num = item.length
+        choice = rand(num)
+        code << item[choice]
     end
+    self.code = code
+
   end
+
+  #Narrows option pool based on past guesses and hints(receives array of matches from exact_match method in Gameplay module
+  def adjust_options(matches)
+
+    matches.each_with_index do |item, index|
+      if item != nil
+        self.options[index] = [item]
+      else
+        self.options[index].delete(code[index])
+      end
+    end
+    
+  end
+
 end
+binding.pry
+puts "goodbye"
